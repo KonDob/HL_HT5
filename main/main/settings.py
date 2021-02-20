6969cadc07ecdb5fe715af0b744d1a5725c2c2ee
KonDob <43993390+KonDob@users.noreply.github.com>
2021-02-20 13:24:00 +0200

    Cache Student View page. Add django-redis to poetry (#11)

--- main/main/settings.py
+++ main/main/settings.py
@@ -86,6 +86,15 @@ DATABASES = {
     }
 }
 
+CACHES = {
+    'default':{
+        'BACKEND': 'django_redis.cache.RedisCache',
+        'LOCATION': 'redis://127.0.0.1:6379/1'
+    }
+}
+
+CACHE_TTL = 60 * 15
+
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
